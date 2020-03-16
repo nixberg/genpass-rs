67044a5c64d134f430b36af6dfc3851da3e682a2
nixberg <k.nixberg@gmail.com>
2020-03-16 12:23:44 +0100

    Initial

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+extern crate rand;
+
+fn main() {
+    use rand::Rng;
+
+    const PASSWORD_LEN: usize = 25;
+    const CHARSET: &[u8] = b"abcdefghijklmnopqrstuvwxyz0123456789";
+
+    let mut rng = rand::thread_rng();
+
+    let password: String = (0..PASSWORD_LEN)
+        .map(|_| {
+            let index = rng.gen_range(0, CHARSET.len());
+            CHARSET[index] as char
+        })
+        .collect();
+
+    println!("{}", password);
+}
